ca15ed0a70eb592e64c6340aac608171d791f7f6
SeungHyeon <112101677+Winstring08@users.noreply.github.com>
2023-05-01 20:32:55 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+		list.add("joonas");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+
+				System.out.println(name+"??!");
+
+	}
+}  
