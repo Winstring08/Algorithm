d03be4b17965db081c26e7ea01d01f8691b74686
SeungHyeon <112101677+Winstring08@users.noreply.github.com>
2023-05-07 20:02:05 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,18 +1,14 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+
 public class Main {
- 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		br.close();
- 
+		int N = Integer.valueOf(br.readLine());
 		StringBuilder sb = new StringBuilder();
- 
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N - i; j++) {
+		for (int i=1; i<=N; i++) {
+			for (int j = 1; j <= N-i; j++) {
 				sb.append(' ');
 			}
 			for (int k = 1; k <= i; k++) {
@@ -22,4 +18,5 @@ public class Main {
 		}
 		System.out.print(sb);
 	}
+	
 }
